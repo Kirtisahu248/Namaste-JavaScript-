79019769a4a3f72940944bc4b3b99b20bb04fdb0
Kirti Sahu <138589688+Kirtisahu248@users.noreply.github.com>
2024-06-07 22:16:29 +0530

    Add files via upload

---
+++ Aboutfun.js
@@ -0,0 +1,23 @@
+//function statement aka function declaration
+
+function a(){
+    console.log("a called");
+}
+//function expression
+
+var b = function(){
+    console.log("b called");
+}
+
+//anonymous function : function without name ,it doesnot have its own identity i.e according to the rules the function shold have names
+
+
+//Anonymous functions are use where functions are used as values
+
+// function () {
+
+// }
+//Named function Expression
+//Difference between parameters and arguments?
+//first class functions
+//Arrow functions
