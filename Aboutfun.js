c15893adcdc8e6feb6fb1d8fded984805ca0176a
Kirti Sahu <138589688+Kirtisahu248@users.noreply.github.com>
2024-06-12 23:09:34 +0530

    Update Aboutfun.js

--- Aboutfun.js
+++ Aboutfun.js
@@ -9,15 +9,41 @@ var b = function(){
     console.log("b called");
 }
 
+b();
+
+
 //anonymous function : function without name ,it doesnot have its own identity i.e according to the rules the function shold have names
 
 
-//Anonymous functions are use where functions are used as values
+//Anonymous functions are used in a place where functions are used as values
 
 // function () {
 
 // }
+
+
 //Named function Expression
+
+// example:
+// var b = function xyz() {
+    // console.log("b called");
+// }
+// xyz(); -------> it will give us error
+// we can call the function inside the function i.e:
+// var b = function(){
+//     console.log(xyz);
+// }
+
 //Difference between parameters and arguments?
-//first class functions
-//Arrow functions
+
+// var b = function(param1 , param2){  ---->these are parameters
+    // console.log("b called");
+// }
+// b(1,2) ;  ------> arguments
+
+
+
+
+//first class functions :  ability to be used like values
+
+//first class citizens   same thing i.e. first class functions
